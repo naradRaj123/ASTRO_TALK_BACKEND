aec5e4f37812b72af24d4be80a44bfddadcc3635
naradraj123 <naradrajbhargzp123@gmail.com>
2025-04-10 12:17:50 +0530

    login sucessfull

--- App/routes/userRoute/index.js
+++ App/routes/userRoute/index.js
@@ -7,5 +7,5 @@ const userController=require('../../controller/userController/index')
 
 
 router.post('/web/register',userController.userRegister);
-
+router.post('/web/login',userController.loginUser);
 module.exports=router
