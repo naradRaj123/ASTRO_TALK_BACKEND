dc79971f4107da6a5bc0447fb7469a712de388a3
naradraj123 <naradrajbhargzp123@gmail.com>
2025-06-01 11:37:56 +0530

    astrolist

--- App/routes/astrologerRoute/index.js
+++ App/routes/astrologerRoute/index.js
@@ -7,6 +7,11 @@ const astroController=require('../../controller/astrologer/index')
 const upload=require('../../multer');
 
 router.post('/web/astro',upload.fields([{ name: 'profileimg', maxCount: 1 }, { name: 'govDoc', maxCount: 1 }]),astroController.registerAstrologer)
-router.post('/web/astro/login',astroController.loginAstro)
+router.post('/web/astro/login',astroController.loginAstro);
+router.post('/web/astro/loginMob',astroController.loginWithMobile);
+router.get('/web/astro/astrolist',astroController.astrolist);
+
+// astrologer audio call
+// router.post('/api/start-call',astroController.astroCall)
 
 module.exports=router;
