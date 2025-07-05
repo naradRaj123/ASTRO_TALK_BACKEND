b969ddf24b3606eb7671a773b50b8bb75c4fb0f4
naradraj123 <naradrajbhargzp123@gmail.com>
2025-07-05 12:32:51 +0530

    update user profile

--- App/routes/userRoute/index.js
+++ App/routes/userRoute/index.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 const express=require('express');
 const router=express.Router();
-
+const upload=require('../../multer');
 const userController=require('../../controller/userController/index')
 
 
 router.post('/web/register',userController.userRegister);
 router.post('/web/login',userController.loginUser);
 router.post('/web/userDataById/:id',userController.UserInfoById);
-router.post('/web/user/update',userController.EditByUserId);
+router.post('/web/user/update',upload.single('user_image'),userController.EditByUserId);
 router.get('/web/user/userlist',userController.Userlist)
 
 //admin api
